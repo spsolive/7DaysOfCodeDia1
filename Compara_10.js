289b7cff895cdeec14a02c7088b6c0ccf8460d9e
spsolive <130874237+spsolive@users.noreply.github.com>
2025-02-26 17:01:50 -0300

    Add files via upload

---
+++ Compara_10.js
@@ -0,0 +1,8 @@
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
---
+++ Compara_30.js
@@ -0,0 +1,8 @@
+let numeroTrinta = 30
+let stringTrinta = '30'
+
+if(numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e o mesmo tipo')
+} else {
+    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
